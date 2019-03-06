cca7a11d73039e44339a6d1feda3d1b63b3e5fd3
isurulakshitha337 <ilakshitha337@gmail.com>
2019-03-06 14:36:37 +0530

    Implement Handler

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -1,13 +1,33 @@
 package lk.ac.kln.countdowntimer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countdown();
     }
+
+    private void countdown() {
+        final TextView textView = findViewById(R.id.textView);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(Integer.toString(counter));
+                counter--;
+                handler.postDelayed(this,1000);
+            }
+        });
+
+    }
+
 }
+
